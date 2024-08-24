ea0564abdf8496f05e1db7242e34d0a03ad4b2d1
Kamrul-Haque <utchas903@gmail.com>
2024-08-24 21:34:04 +0600

    Imports fixed in global exception handlers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
                   ->withRouting(
