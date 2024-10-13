e4a03c33fa27f78a87b73dc93cfcf38bd8205759
Kamrul-Haque <utchas903@gmail.com>
2024-10-13 14:07:30 +0600

    flash messages fixed

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -24,23 +24,15 @@ watch(show, () => {
 
 <template>
     <div v-if="flash.success && show"
-         class="flex justify-between bg-success opacity-25 border-2 border-success rounded-lg p-4 bottom-10 fixed right-5 z-10 text-sm text-success w-full md:w-1/3 break-all"
+         class="flex items-center bg-success bg-opacity-15 rounded-lg p-4 bottom-20 fixed right-4 z-10 text-sm text-success w-full md:w-1/3 break-all"
          role="alert">
-        <div class="flex-1">
-            <span class="mdi mdi-check-circle text-lg mr-4"></span>
-            {{ flash.success }}
-        </div>
-        <span class="mdi mdi-close-circle-outline text-lg ml-4"
-              @click="show=false"></span>
+        <i class="mdi mdi-check-circle text-lg mr-4"></i>
+        {{ flash.success }}
     </div>
     <div v-if="flash.error && show"
-         class="flex justify-between bg-error opacity-25 border-2 border-error rounded-lg p-4 bottom-10 fixed right-5 z-10 text-sm text-error w-full md:w-1/3 break-all"
+         class="flex items-center bg-error bg-opacity-15 rounded-lg p-4 bottom-20 fixed right-4 z-10 text-sm text-error w-full md:w-1/3 break-all"
          role="alert">
-        <div class="flex-1">
-            <span class="mdi mdi-alert-circle text-lg mr-4"></span>
-            {{ flash.error }}
-        </div>
-        <span class="mdi mdi-close-circle-outline text-lg ml-4"
-              @click="show=false"></span>
+        <i class="mdi mdi-alert-circle text-lg mr-4"></i>
+        {{ flash.error }}
     </div>
 </template>
